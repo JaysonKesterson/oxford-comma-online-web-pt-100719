874414dc4afeff247fffd187890d677336d60cc3
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-14 14:25:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  
+  counter = 0
+  
+ if array.length == 1 
+   array.join
+ elsif array.length == 2
+   array[0] = array[0] + " and "
+   array.join
+ elsif array.lentgh == 3
+   array[0] = array[0] + ", "
+   array[1] = array[1] + ", and "
+   array.join
+ else 
+   while array.lenth < counter do
+     array[counter] = array [counter] + 
+   end
+ end
 end
